2fb1295bee2f8aec50c90d4e7c5a7dbf0b2cc0aa
Derek Pappas <depappas@sifive.com>
2019-05-21 23:43:14 -0700

    add new ram ltn nodes

---
+++ src/main/scala/diplomaticobjectmodel/model/OMSRAM.scala
@@ -0,0 +1,12 @@
+// See LICENSE.SiFive for license details.
+
+package freechips.rocketchip.diplomaticobjectmodel.model
+
+case class OMSRAM(
+  description: String,
+  addressWidth: Int,
+  dataWidth: Int,
+  depth: BigInt,
+  writeMaskGranularity: Int,
+  _types: Seq[String] = Seq("OMSRAM")
+) extends OMComponent
